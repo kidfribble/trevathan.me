efc9cb6e1b88525647c9b192abcdbeff3c84675e
Steven Trevathan <steven@makemodel.co>
2024-01-10 18:05:14 -0800

    intersection observer added for resume menu

--- scripts/window-observer.js
+++ scripts/window-observer.js
@@ -7,7 +7,7 @@ let observer = new IntersectionObserver(entries => {
   if (entries[0].boundingClientRect.y < 0) {
     document.body.classList.add("toc-top");
   } else {
-    document.body.classList.remove("toc-follow");
+    document.body.classList.remove("toc-top");
   }
 });
 observer.observe(document.querySelector("#makemodelPositioner"));
