fa12cdff2c4d394ae9900e01c38670860e6c8664
Steven Trevathan <steven@makemodel.co>
2024-01-11 18:33:56 -0800

    possible preloader fix

--- scripts/img-rotation.js
+++ scripts/img-rotation.js
@@ -1,7 +1,5 @@
-// The debounce function receives our function as a parameter
 const debounce = (fn) => {
 
-  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
   let frame;
 
   // The debounce function returns a new function that can receive a variable number of arguments
@@ -22,9 +20,6 @@ const debounce = (fn) => {
   } 
 };
 
-
-// Reads out the scroll position and stores it in the data attribute
-// so we can use it in our stylesheets
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
 
@@ -59,11 +54,8 @@ const threshholds = () => {
   }
 }
 
-// Listen for new scroll events, here we debounce our `storeScroll` function
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
-
-// Update scroll position for first time
 storeScroll();
 threshholds();
 
