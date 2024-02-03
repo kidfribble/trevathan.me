1575f44122c624450f5ad6f077354ddd312057b2
Steven Trevathan <steven@makemodel.co>
2024-02-02 16:36:24 -0800

    sending portfolio

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,21 @@
+const fg = require('fast-glob');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
+
+  const portfolioImages = fg.sync(['**images/portfolio/image-dump/**']);
+
+  //Create collection of portfolio images
+  eleventyConfig.addCollection('portfolioAssets', function(collection) {
+    return portfolioImages;
+  });
+
+  const logoParade = fg.sync(['**images/portfolio/logo-parade/**']);
+
+  //Create collection of portfolio images
+  eleventyConfig.addCollection('logos', function(collection) {
+    return logoParade;
+  });
 
   eleventyConfig.addPassthroughCopy("fonts");
 
@@ -7,9 +23,17 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("scripts");
 
+  eleventyConfig.addGlobalData("navigation",
+    [
+      ["History", "/"],
+      ["Notes", "/notes"],
+      ["Portfolio", "/portfolio"]
+    ]);
+
   const { DateTime } = require("luxon");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
 };
---
+++ portfolio/tator.md
@@ -0,0 +1,13 @@
+---
+title: Tator - by CVision
+layout: portfolio-detail.njk
+date: 2024-01-21
+tags: ['portfolio','UX', 'UI']
+artsrc: ['../images/portfolio/Tator-Overview.png','../images/portfolio/Tator-Review.png', '../images/portfolio/Tator-Management.png']
+---
+
+## Product Design Lead / Creative Director
+
+As the principle designer of Make&Model I worked with the CVision team and their customers to conceive of a user experience that would allow marine biologists and researchers to much more efficiently transform huge amounts of video data into actionable insights. Later in the project I worked with a brand designer from my team at Make&Model to work out the core brand tones that the website and the product would take on. The brand direction of this is the product of our collaboration, while the overall product design and organizational experience are my sole work product.
+
+&#x263B;
